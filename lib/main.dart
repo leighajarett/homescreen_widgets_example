f3b6f7205c2c4e7b5ce414b85271fa96a6002c7e
Eric Windmill <ewindmill@google.com>
2022-12-28 13:41:42 -0800

    checkin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:homescreen_widgets/news_data.dart';
 
+import 'color_schemes.g.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         useMaterial3: true,
+        colorScheme: lightColorScheme,
       ),
       home: const MyHomePage(),
     );
@@ -25,20 +28,49 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('HomeScreen Widgets Workshop')),
-      body: Center(
-        child: ListView.builder(
-          itemBuilder: (context, idx) {
-            final article = getNewsStories()[idx];
-            return ListTile(
-              key: Key("$idx ${article.hashCode}"),
-              title: Text(article.title!),
-              subtitle: Text(article.description!),
-              onTap: () {
-                _showArticlePage(context, article);
-              },
-            );
-          },
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Image.asset('images/FlutterForward_Logo_Dark_Gradient.png'),
+            const SizedBox(height: 30),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.tertiaryContainer,
+              ),
+              child: Row(
+                children: [
+                  Text(
+                    'Top Stories',
+                    textAlign: TextAlign.left,
+                    style: Theme.of(context).textTheme.headlineLarge!,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 25),
+            Expanded(
+              child: ListView.separated(
+                separatorBuilder: (context, idx) {
+                  return const Divider();
+                },
+                itemCount: getNewsStories().length,
+                itemBuilder: (context, idx) {
+                  final article = getNewsStories()[idx];
+                  return ListTile(
+                    key: Key("$idx ${article.hashCode}"),
+                    title: Text(article.title!),
+                    subtitle: Text(article.description!),
+                    onTap: () {
+                      _showArticlePage(context, article);
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -50,12 +82,30 @@ class MyHomePage extends StatelessWidget {
       MaterialPageRoute(builder: (context) {
         return Material(
           child: Scaffold(
-            body: Center(
+            appBar: AppBar(
+              title: Text(article.title!),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(article.title!),
-                  Text(article.description!),
-                  const Text('TODO: Lorem Ipsum')
+                  if (article.image != null)
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Center(
+                        child: Image.asset('images/${article.image}'),
+                      ),
+                    ),
+                  const SizedBox(height: 10.0),
+                  Text(article.description!,
+                      style: Theme.of(context).textTheme.titleMedium),
+                  const SizedBox(height: 20.0),
+                  Text(article.articleText),
+                  const SizedBox(height: 8),
+                  Text(article.articleText),
+                  const SizedBox(height: 8),
+                  Text(article.articleText),
                 ],
               ),
             ),
--- lib/news_data.dart
+++ lib/news_data.dart
@@ -7,17 +7,23 @@ class NewsArticle {
   NewsArticle({
     this.title,
     this.description,
+    this.image,
   });
+
+  final String articleText =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+  final String? image;
 }
 
 List<NewsArticle> getNewsStories() {
   List<NewsArticle> news = [];
   news = [
     NewsArticle(
-      title: "Announcing Flutter Forward",
-      description:
-          "Flutter Forward is taking place in Nairobi, Kenya in January 2023",
-    ),
+        title: "Announcing Flutter Forward",
+        description:
+            "Flutter Forward is taking place in Nairobi, Kenya in January 2023",
+        image: "Flutter_Forward_Teaser_Still_1x1_v01_small.jpg"),
     NewsArticle(
       title: "Flutter Forward Keynote announcements",
       description:
