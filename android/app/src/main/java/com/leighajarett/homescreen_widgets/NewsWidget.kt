52f7e82397839af39e3cf5cdf3a5e01655b63c58
Eric Windmill <ewindmill@google.com>
2022-12-29 09:26:03 -0800

    adds android code to widget 1

--- android/app/src/main/java/com/leighajarett/homescreen_widgets/NewsWidget.kt
+++ android/app/src/main/java/com/leighajarett/homescreen_widgets/NewsWidget.kt
@@ -1,24 +1,56 @@
 package com.leighajarett.homescreen_widgets
 
 import android.appwidget.AppWidgetManager
-import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
 import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetBackgroundIntent
+import es.antonborri.home_widget.HomeWidgetLaunchIntent
+import es.antonborri.home_widget.HomeWidgetProvider
 
 /**
  * Implementation of App Widget functionality.
  */
-class NewsWidget : AppWidgetProvider() {
+class NewsWidget : HomeWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
+        appWidgetIds: IntArray,
+        widgetData: SharedPreferences
     ) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            val views = RemoteViews(context.packageName, R.layout.news_widget).apply {
+                // Open App on Widget Click
+                val pendingIntent = HomeWidgetLaunchIntent.getActivity(
+                    context,
+                    MainActivity::class.java)
+                setOnClickPendingIntent(R.id.widget_container, pendingIntent)
+
+                // Swap Title Text by calling Dart Code in the Background
+                setTextViewText(R.id.headline_title, widgetData.getString("headline_title", null)
+                    ?: "No Title Set")
+                val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(
+                    context,
+                    Uri.parse("homewidgetexample://titleclicked")
+                )
+                setOnClickPendingIntent(R.id.headline_title, backgroundIntent)
+
+                val message = widgetData.getString("headline_description", null)
+                setTextViewText(R.id.headline_description, message
+                    ?: "No Message Set")
+                // Detect App opened via Click inside Flutter
+                val pendingIntentWithData = HomeWidgetLaunchIntent.getActivity(
+                    context,
+                    MainActivity::class.java,
+                    Uri.parse("homewidgetexample://message?message=$message"))
+                setOnClickPendingIntent(R.id.headline_description, pendingIntentWithData)
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
+
 
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
@@ -27,18 +59,4 @@ class NewsWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
-
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.news_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
 }
