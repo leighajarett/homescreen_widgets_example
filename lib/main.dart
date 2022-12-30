ecfd2a282e380b8ad5798f652286805573c48f35
Eric Windmill <ewindmill@google.com>
2022-12-30 09:34:55 -0800

    make code simpler

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:home_widget/home_widget_callback_dispatcher.dart';
 import 'package:homescreen_widgets/chart_page.dart';
-import 'package:homescreen_widgets/homescreen_utils.dart';
 import 'package:homescreen_widgets/news_data.dart';
 import 'package:workmanager/workmanager.dart';
 
@@ -44,8 +43,16 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     // Set the group ID
     HomeWidget.setAppGroupId('group.leighawidget');
+
     // Mock read in some data and update the headline
-    HomescreenUtils.updateHeadline(getNewsStories()[0]);
+    final newHeadline = getNewsStories()[0];
+    HomeWidget.saveWidgetData<String>('headline_title', newHeadline.title);
+    HomeWidget.saveWidgetData<String>(
+        'headline_description', newHeadline.description);
+    HomeWidget.updateWidget(
+      iOSName: 'NewsWidgets',
+      androidName: 'NewsWidget',
+    );
   }
 
   int _selectedIndex = 0;
